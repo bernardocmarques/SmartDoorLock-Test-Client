c0317ecfeffef4fe5659ab315129d5af5e74c614
bernardocmarques <bernardocmarques@outlook..pt>
2022-03-04 17:55:51 +0000

    Timestamp message verification

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import base64
 import hmac
 import hashlib
 import time
+import traceback
 
 from aes_util import AES_Util
 from rsa_util import RSA_Util
 
 AES: AES_Util
 
-door_ip = '192.168.1.139'  # Door ip (Change if necessary)
+door_ip = '192.168.1.112'  # Door ip (Change if necessary)
 door_port = 3333  # Door port (Change if necessary)
 
 rsa = RSA_Util("public_key.pem")  # Door public key. (Hardcoded in the server)
@@ -22,6 +23,13 @@ master_key = "SoLXxAJHi1Z3NKGHNnS5n4SRLv5UmTB4EssASi0MmoI="
 client_times = []
 server_t1 = []
 server_t2 = []
+
+
+def create_message_ts_and_nonce(message):
+    timeframe = 30
+    now = int(time.time())
+    # return message
+    return f"{message} {(now - int(timeframe / 2))} {(now + int(timeframe / 2))}"
 
 
 def get_session_credentials_base64():
@@ -64,7 +72,7 @@ while i < n:
             hmac.new(base64.b64decode(master_key), base64.b64decode(seed), hashlib.sha256).digest()).decode()
 
         # Send authorization credentials. "SAC user_id authorization_code"
-        sock.send((AES.encrypt(f"SAC {user_id} {auth_code}")).encode())
+        sock.send((AES.encrypt(create_message_ts_and_nonce(f"SAC {user_id} {auth_code}"))).encode())
 
         # Receive confirmation of authorization. "ACK"
         res = sock.recv(1024)
@@ -72,18 +80,18 @@ while i < n:
         confirmation = AES.decrypt(msg, iv)
 
         # Send request to unlock door. "RUD"
-        sock.send((AES.encrypt(f"RUD")).encode())
+        sock.send((AES.encrypt(create_message_ts_and_nonce(f"RUD"))).encode())
 
         # Receive confirmation of unlocking. "ACK"
         res = sock.recv(1024)
         msg, iv = res.decode().split(" ")
-        confirmation, t1, t2 = AES.decrypt(msg, iv).split(" ")
+        confirmation = AES.decrypt(msg, iv).split(" ")
 
         # Stop client side timer and print time elapsed
         end = time.time()
         client_times.append(end - start)
-        server_t1.append(int(t1))
-        server_t2.append(int(t2))
+        # server_t1.append(int(t1))
+        # server_t2.append(int(t2))
 
         if i % 25 == 0:
             print(f"Test {i}:\n")
@@ -91,8 +99,12 @@ while i < n:
         # Close connection to door
         sock.close()
         i += 1
-    except:
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+
         print(f"Error! Retrying, i={i}...")
+        exit()
         pass
 
 print(client_times)
